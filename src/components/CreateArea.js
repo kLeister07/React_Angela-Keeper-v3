b2dbab7c83c68d7d1623bc47f7dcb2f0d14aeeea
Kevin Leister <kevinleister07@gmail.com>
2022-12-05 18:56:03 -0600

    add styled components

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,6 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import Fab from '@mui/material/Fab';
+// Fab adds hover effect
+import Zoom from '@mui/material/Zoom';
+// zoom animates anything it wraps with a zoom effect, uses a prop to identify how to function
 
 function CreateArea(props) {
+
+const [isExpanded, setIsExpanded] = useState(false)
+
   const [note, setNote] = useState({
     title: '',
     content: ''
@@ -25,23 +33,32 @@ function CreateArea(props) {
     });
   }
 
+  function expand() {
+    setIsExpanded(true)
+  }
+
     return (
         <div>
             <form className='create-note'>
-                <input
+{isExpanded && <input
                     onChange={handleChange}
                     value={note.title}
                     name='title'
                     placeholder='Title'
-                />
+                />}
                 <textarea
-                    onChange={handleChange}
+            onChange={handleChange}
+            onClick={expand}
                     value={note.content}
                     name='content'
                     placeholder='Take a note...'
-                    rows='3'
+                    rows={isExpanded ? 3 : 1}
                 />
-                <button onClick={submitNote}>Add</button>
+                <Zoom in={isExpanded}>
+                    <Fab onClick={submitNote}>
+                        <NoteAddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
